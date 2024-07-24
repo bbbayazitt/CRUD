ed3c855ea82dd37e3c25d1090bd18ae4f6b43282
bbbayazitt <berkay01907@gmail.com>
2024-07-23 22:41:41 -0400

    connection string added , dbcontext class created

---
+++ CRUD/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+    }
+}
