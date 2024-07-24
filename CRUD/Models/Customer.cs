f013d94cd2e49c1265f99b099f0cf558145edf7b
bbbayazitt <berkay01907@gmail.com>
2024-07-23 20:43:19 -0400

    customer class created

--- CRUD/Models/Customer.cs
+++ CRUD/Models/Customer.cs
@@ -2,5 +2,9 @@
 {
     public class Customer
     {
+        public int Id { get; set; } // entity framework know automatically its primary key
+        public string? FirstName { get; set; } // ? means that nullable
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
     }
 }
