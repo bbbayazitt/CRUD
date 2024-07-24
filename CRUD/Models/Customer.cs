558c70113feb9caf952435ae1132887bffd936a9
bbbayazitt <berkay01907@gmail.com>
2024-07-24 00:20:14 -0400

    HttpPost method added

--- CRUD/Models/Customer.cs
+++ CRUD/Models/Customer.cs
@@ -1,10 +1,17 @@
-﻿namespace CRUD.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
 {
     public class Customer
     {
         public int Id { get; set; } // entity framework know automatically its primary key
+        [Required, MinLength(2, ErrorMessage = "First name is too short")]
+        [DisplayName("First Name")]
         public string? FirstName { get; set; } // ? means that nullable
+        [Required, MinLength(2, ErrorMessage = "First name is too short") ]
         public string? LastName { get; set; }
+        [Required, EmailAddress]
         public string? Email { get; set; }
     }
 }
